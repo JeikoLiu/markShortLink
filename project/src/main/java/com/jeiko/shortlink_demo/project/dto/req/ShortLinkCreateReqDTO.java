ae7feeab7423e1d66c69eb55f109dc32c646639a
JeikoLiu <1959670344@qq.com>
2024-09-10 16:59:45 +0800

    feature: 前端传递有效期时间的序列化

--- project/src/main/java/com/jeiko/shortlink_demo/project/dto/req/ShortLinkCreateReqDTO.java
+++ project/src/main/java/com/jeiko/shortlink_demo/project/dto/req/ShortLinkCreateReqDTO.java
@@ -1,5 +1,6 @@
 package com.jeiko.shortlink_demo.project.dto.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -43,6 +44,7 @@ public class ShortLinkCreateReqDTO {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8" )
     private Date validDate;
 
     /**
