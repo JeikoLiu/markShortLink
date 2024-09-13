fd486773b6b2522e518421a6408f693d52e8dcd1
JeikoLiu <1959670344@qq.com>
2024-09-13 22:27:54 +0800

    feature: 开发短链接监控功能——基础访问UV数据

--- project/src/main/java/com/jeiko/shortlink_demo/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/jeiko/shortlink_demo/project/service/impl/ShortLinkServiceImpl.java
@@ -2,7 +2,9 @@ package com.jeiko.shortlink_demo.project.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.text.StrBuilder;
+import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -30,6 +32,8 @@ import com.jeiko.shortlink_demo.project.utils.HashUtils;
 import com.jeiko.shortlink_demo.project.utils.LinkUtils;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -47,11 +51,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.jeiko.shortlink_demo.project.common.constant.RedisKeyConstant.*;
 
@@ -289,7 +291,34 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
      * 监控短链接访问
      */
     private void shortLinkStats(ServletRequest request, ServletResponse response, String fullShortUrl, String gid) {
+        // 原子性的标志位,用于判断用户是否是第一次访问改短链接
+        AtomicBoolean uvFirstFlag = new AtomicBoolean();
+        Cookie[] cookies = ((HttpServletRequest)request).getCookies();
         try {
+            Runnable addResponseCookie = () -> {
+                String uv = UUID.fastUUID().toString();
+                Cookie uvCookie = new Cookie("uv", uv);
+                // 设置cookie的时间为一天
+                uvCookie.setMaxAge(60 * 60 * 24);
+                // 设置用户下次访问该短链接时就携带该cookie
+                uvCookie.setPath(StrUtil.sub(fullShortUrl, fullShortUrl.indexOf('/'), fullShortUrl.length()));
+                ((HttpServletResponse)response).addCookie(uvCookie);
+                // 新用户则置为一
+                uvFirstFlag.set(Boolean.TRUE);
+                stringRedisTemplate.opsForSet().add("short-link:stats:uv" + fullShortUrl, uv);
+            };
+            if (ArrayUtil.isNotEmpty(cookies)) {
+                Arrays.stream(cookies)
+                        .filter(each -> Objects.equals(each.getName(), "uv"))
+                        .findFirst()
+                        .map(Cookie::getValue)
+                        .ifPresentOrElse(each -> {
+                            Long added = stringRedisTemplate.opsForSet().add("short-link:stats:uv" + fullShortUrl, each);
+                            uvFirstFlag.set(added != null && added > 0L);
+                        }, addResponseCookie);
+            } else {
+                addResponseCookie.run();
+            }
             if (StrUtil.isBlank(gid)) {
                 LambdaQueryWrapper<ShortLinkGotoDO> queryWrapper = Wrappers.lambdaQuery(ShortLinkGotoDO.class)
                         .eq(ShortLinkGotoDO::getFullShortUrl, fullShortUrl);
@@ -306,7 +335,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                     .gid(gid)
                     .date(date)
                     .pv(1)
-                    .uv(1)
+                    .uv(uvFirstFlag.get() ? 1 : 0)
                     .uip(1)
                     .build();
             linkAccessStatsMapper.shortLinkStats(linkAccessStatsDO);
