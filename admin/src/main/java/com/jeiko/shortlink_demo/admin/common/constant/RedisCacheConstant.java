152819e23d3fd2ab549ee7a673ba3993029ac770
JeikoLiu <1959670344@qq.com>
2024-09-19 20:18:44 +0800

    optimize: 优化单个用户创建分组数不能超过20

--- admin/src/main/java/com/jeiko/shortlink_demo/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/com/jeiko/shortlink_demo/admin/common/constant/RedisCacheConstant.java
@@ -12,4 +12,9 @@ public class RedisCacheConstant {
      */
     public static final String USER_LOGIN_KEY = "short-link:login:";
 
+    /**
+     * 分组创建分布式锁
+     */
+    public static final String LOCK_GROUP_CREATE_KEY = "short-link:lock_group-create:%s";
+
 }
--- admin/src/main/java/com/jeiko/shortlink_demo/admin/service/imp/GroupServiceImpl.java
+++ admin/src/main/java/com/jeiko/shortlink_demo/admin/service/imp/GroupServiceImpl.java
@@ -1,11 +1,13 @@
 package com.jeiko.shortlink_demo.admin.service.imp;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.jeiko.shortlink_demo.admin.common.biz.user.UserContext;
+import com.jeiko.shortlink_demo.admin.common.convention.exception.ClientException;
 import com.jeiko.shortlink_demo.admin.common.convention.result.BaseResult;
 import com.jeiko.shortlink_demo.admin.dao.entity.GroupDO;
 import com.jeiko.shortlink_demo.admin.dao.mapper.GroupMapper;
@@ -18,12 +20,17 @@ import com.jeiko.shortlink_demo.admin.service.GroupService;
 import com.jeiko.shortlink_demo.admin.utils.RandomGenerator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+import static com.jeiko.shortlink_demo.admin.common.constant.RedisCacheConstant.LOCK_GROUP_CREATE_KEY;
 
 /**
  * 短链接分组接口实现
@@ -32,6 +39,11 @@ import java.util.stream.Collectors;
 @Slf4j
 @RequiredArgsConstructor
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+
+    private final RedissonClient redissonClient;
+
+    @Value("${short-link:group:max-num}")
+    private Integer groupMaxNum;
 
     /**
      * 后续重构为 SpringCloud Feign 调用
@@ -46,17 +58,30 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     @Override
     public void saveGroup(String username, String groupName) {
-        String gid;
-        do {
-            gid = RandomGenerator.generateRandom();
-        } while (!hasGid(username, gid));
-        GroupDO groupDO = GroupDO.builder()
-                .gid(gid)
-                .sortOrder(0)
-                .username(username)
-                .name(groupName)
-                .build();
-        baseMapper.insert(groupDO);
+        RLock lock = redissonClient.getLock(String.format(LOCK_GROUP_CREATE_KEY, username));
+        lock.lock();
+        try {
+            LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                    .eq(GroupDO::getUsername, username)
+                    .eq(GroupDO::getDelFlag, 0);
+            List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+            if (CollUtil.isNotEmpty(groupDOList) && groupDOList.size() == groupMaxNum) {
+                throw new ClientException(String.format("已超出最大分组数：%d", groupMaxNum));
+            }
+            String gid;
+            do {
+                gid = RandomGenerator.generateRandom();
+            } while (!hasGid(username, gid));
+            GroupDO groupDO = GroupDO.builder()
+                    .gid(gid)
+                    .sortOrder(0)
+                    .username(username)
+                    .name(groupName)
+                    .build();
+            baseMapper.insert(groupDO);
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
