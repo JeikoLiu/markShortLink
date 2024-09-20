bac95f1c3fda18002cd89a58f7577c2d68b2f2d3
JeikoLiu <1959670344@qq.com>
2024-09-20 15:50:35 +0800

    optimize: 缓存 KEY 移至常量类

--- project/src/main/java/com/jeiko/shortlink_demo/project/mq/producer/ShortLinkStatsSaveProducer.java
+++ project/src/main/java/com/jeiko/shortlink_demo/project/mq/producer/ShortLinkStatsSaveProducer.java
@@ -1,11 +1,12 @@
 package com.jeiko.shortlink_demo.project.mq.producer;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+
+import static com.jeiko.shortlink_demo.project.common.constant.RedisKeyConstant.SHORT_LINK_STATS_STREAM_TOPIC_KEY;
 
 /**
  * 短链接监控数据保存消息队列生产者
@@ -15,14 +16,11 @@ import java.util.Map;
 public class ShortLinkStatsSaveProducer {
     private final StringRedisTemplate stringRedisTemplate;
 
-    @Value("${spring.data.redis.channel-topic.short-link-stats}")
-    private String topic;
-
     /**
      * 发送延迟消费短链接统计
      */
     public void send(Map<String, String> producerMap) {
-        stringRedisTemplate.opsForStream().add(topic, producerMap);
+        stringRedisTemplate.opsForStream().add(SHORT_LINK_STATS_STREAM_TOPIC_KEY, producerMap);
     }
 
 }
